79fa3f14651601471629828e3f20e0aa45640783
Bruno <brunopassos_bp@hotmail.com>
2022-12-01 15:49:37 -0300

    feat: list client contacts implemented

--- server/src/routes/contacts/contacts.routes.ts
+++ server/src/routes/contacts/contacts.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import createContactController from "../../controllers/contacts/createContact.controller";
+import listClientContactsController from "../../controllers/contacts/listClientContacts.controller";
 
 
 const contactsRoutes = Router();
 
 contactsRoutes.post("/contacts/register", createContactController);
+contactsRoutes.get("/contacts/:id", listClientContactsController);
 
 export default contactsRoutes;
