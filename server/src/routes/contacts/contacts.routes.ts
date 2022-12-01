984bdecaebc53f81de3e14a31b4643f36e131a32
Bruno <brunopassos_bp@hotmail.com>
2022-12-01 15:57:23 -0300

    feat: list one contact impemented

--- server/src/routes/contacts/contacts.routes.ts
+++ server/src/routes/contacts/contacts.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import createContactController from "../../controllers/contacts/createContact.controller";
 import listClientContactsController from "../../controllers/contacts/listClientContacts.controller";
+import listOneContactController from "../../controllers/contacts/listOneContact.controller";
 
 
 const contactsRoutes = Router();
 
 contactsRoutes.post("/contacts/register", createContactController);
-contactsRoutes.get("/contacts/:id", listClientContactsController);
+contactsRoutes.get("/contacts/client/:id", listClientContactsController);
+contactsRoutes.get("/contacts/:id", listOneContactController);
 
 export default contactsRoutes;
---
+++ server/src/services/contacts/listOneContact.service.ts
@@ -0,0 +1,17 @@
+import { AppDataSource } from "../../data-source";
+import { Contacts } from "../../entities/contacts.entity";
+import { AppError } from "../../errors/appError";
+
+const listOneContactService = async (id: string): Promise<Contacts> => {
+    const contactsRepository = AppDataSource.getRepository(Contacts);
+
+    const contact = await contactsRepository.findOneBy({id:id});
+
+    if(!contact){
+        throw new AppError(404, "Contact noew Found");
+    }
+
+    return contact;
+}
+
+export default listOneContactService
